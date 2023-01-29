f87c487bc69c032e9c5aaca466160e1ea003848b
Yan Weschenfelder Rodrigues <yanywr@gmail.com>
2023-01-29 17:06:11 -0300

    feat: added docker and prismas configs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,24 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { PrismaService } from './prisma.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly prisma: PrismaService,
+  ) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async getHello(): Promise<any> {
+    await this.prisma.notification.create({
+      data: {
+        category: '',
+        content: '',
+        id: 'aaaa',
+        userId: 'aaa',
+      },
+    });
+    return await this.prisma.notification.findMany();
   }
 }
